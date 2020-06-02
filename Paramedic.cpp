cdcd4c839d02073aed9b1abb3acf6c36ba6dda33
Eliel Gez <eliel7114@gmail.com>
2020-06-02 19:48:38 +0300

    remove prints

--- Paramedic.cpp
+++ Paramedic.cpp
@@ -4,9 +4,7 @@
 void Paramedic::action(vector<vector<Soldier *>> &board, pair<int, int> location) {
     vector<Soldier *> to_heal = WarGame::Board::getMySoldiers(board, location, player_num);
     for (int i = 0; i < to_heal.size(); i++) {
-        printf("to_heal hp : %d\n", to_heal[i]->hp);
         to_heal[i]->hp = to_heal[i]->getInitHp();
-        printf("to_heal hp after : %d\n", to_heal[i]->hp);
     }
 }
 const uint Paramedic::getInitHp() const {
